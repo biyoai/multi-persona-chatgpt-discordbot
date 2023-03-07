8b8e9c21f9a8b57694d152c70014778546a42cd3
temasaguru <temasaguru@gmail.com>
2023-03-07 23:11:52 +0900

    chore: パラメータ調整

--- src/_utils/config.ts
+++ src/_utils/config.ts
@@ -8,7 +8,7 @@ export const CHAT_GPT_PARAMS: Omit<CreateChatCompletionRequest, 'messages'> = {
   model: 'gpt-3.5-turbo',
   max_tokens: safeEnv.OPENAI_CHAT_GPT_ANSWER_MAX_TOKEN,
   // 上げるとやばい返答が返ってくる
-  temperature: 1.2,
+  temperature: 1.1,
   // 新しい話題を話しづらくなるらしい
-  presence_penalty: -0.6,
+  presence_penalty: -0.3,
 };
